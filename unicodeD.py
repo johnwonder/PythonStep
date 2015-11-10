f4b300f046373baca52490ef1f6c45e4c3032e22
johnwonder <johnufida@163.com>
2015-11-10 22:17:59 +0800

    字符串操作

---
+++ unicodeD.py
@@ -0,0 +1,13 @@
+﻿import unicodedata
+
+title = u"Klüft skräms inför på fédéral électoral große"
+B= unicodedata.normalize('NFKD',title).encode('ascii','ignore')
+
+print(B)
+
+#format字符串http://www.cnblogs.com/cposture/p/4722340.html
+print('number is %d'%10)
+
+
+#字符串转数字
+print(int("234"))
---
+++ 字符串操作.py
@@ -0,0 +1,8 @@
+﻿a = '%6.2f'%1.235
+print(a)
+
+a = '%06.2f'%1.235
+print(a)#用0补
+
+a='%(name)s:%(score)06.1f'%{'score':9.5,'name':'john'}
+print(a)#输出john:0009.5
